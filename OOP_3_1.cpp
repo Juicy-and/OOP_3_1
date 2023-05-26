c0d7497a25db674b272e9ac852a7b2f29980fb92
Andrey <marfajuc@gmail.com>
2023-05-26 21:01:49 +0300

    Добавьте файлы проекта.

---
+++ OOP_3_1.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <string>
+
+class Figure
+{
+protected:
+    int sides_count = 0;
+    int get_sides_count() { return sides_count; }
+    std::string name = "Figure: ";
+    std::string get_name() { return name; }
+public:
+    void method() 
+    { 
+        std::cout << name << sides_count << std::endl; 
+    }
+};
+class Triangle : public Figure
+{
+public:
+     void method1()
+     {
+        sides_count = 3;
+        name = "Triangle: ";
+        std::cout << name << sides_count << std::endl;  
+    }
+};
+class Quadrangle : public Figure
+{
+public:
+    void method2()
+    {
+        sides_count = 4;
+        name = "Quadrangle: ";
+        std::cout << name << sides_count << std::endl;
+    }
+};
+int main()
+{
+    std::cout << "Number of sides:" << std::endl;
+    Figure figure;
+    figure.method();
+    Triangle triangle;
+    triangle.method1();
+    Quadrangle quadrangle;
+    quadrangle.method2();
+}
+
